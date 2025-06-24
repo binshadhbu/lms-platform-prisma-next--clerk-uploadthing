7ae619cf21e62aba563929083072b42f68a9f031
binshadhbu <binshadhubasheer@gmail.com>
2025-06-24 11:47:00 +0530

    setup teacher mode and student mode

--- app/(dashboard)/_components/Navbar.tsx
+++ app/(dashboard)/_components/Navbar.tsx
@@ -1,10 +1,12 @@
 import React from 'react'
 import MobileSidebar from './MobileSidebar'
+import NavbarRoutes from '@/components/NavbarRoutes'
 
 const Navbar = () => {
   return (
     <div className='p-4 border-bottom h-full flex items-center bg-white shadow-sm'>
       <MobileSidebar/>
+      <NavbarRoutes/>
     </div>
   )
 }
