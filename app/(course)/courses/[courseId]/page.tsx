8df0db79514313fb17b385bd9eea189b6dfe2af1
binshadhbu <binshadhubasheer@gmail.com>
2025-07-02 00:14:41 +0530

    implemented course browse page for user and side bar of view course page

---
+++ app/(course)/courses/[courseId]/page.tsx
@@ -0,0 +1,12 @@
+import React from 'react'
+
+const page = () => {
+  return (
+    <div>
+        watch the course
+      
+    </div>
+  )
+}
+
+export default page
--- app/(dashboard)/(routes)/search/page.tsx
+++ app/(dashboard)/(routes)/search/page.tsx
@@ -5,6 +5,7 @@ import SearchInput from '@/components/SearchInput';
 import { GetCourses } from '@/actions/getCourses';
 import { auth } from '@clerk/nextjs/server';
 import { redirect } from 'next/navigation';
+import CourseList from '@/components/CourseList';
 
 interface SearchPageProps {
   searchParams: {
@@ -33,8 +34,9 @@ const page = async ({ searchParams }: SearchPageProps) => {
       <div className='px-6 pt-6 md:hidden md:mb-0 block'>
         <SearchInput />
       </div>
-      <div className='p-6 '>
+      <div className='p-6 space-y-4'>
         <Categories items={categories} />
+        <CourseList items={courses} />
 
       </div>
     </>
